ffed44238aee5e602e585e3bf90781325f7c8517
TagBalu <baluyevslava@icloud.com>
2025-06-17 16:17:02 +0200

    correzioni

--- src/Esr3.java
+++ src/Esr3.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
 
 public class Esr3 {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        System.out.println("Inserisci il primo valore");
-        double LatoA = scanner.nextDouble();
-        System.out.println("Inserisci il secondo valore ");
-        double LatoB = scanner.nextDouble();
-        System.out.println("Inserisci il terzo valore");
-        double LatoC = scanner.nextDouble();
-        System.out.println("Ecco il perimetro del tuo triangolo : " +(LatoA+LatoB+LatoC) );
-        double P=LatoA+LatoB+LatoC;
-        if (P % 2== 0){
-            System.out.println("0 è pari" );
-        } else {
-            System.out.println("1 è dispari");
-        }
-
-        double A=Math.sqrt(P*(P-LatoA)*(P-LatoB)*(P-LatoC));
-        System.out.println("Ecco l'area del triangolo con il metodo di Erone: " + A);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("insersici l1");
+        double l1=scanner.nextDouble();
+        System.out.println("insersici l2");
+        double l2= scanner.nextDouble();
+        double risultato=perimetroRettangolo(l1,l2);
+        System.out.println("il perimetro è: "+ risultato);
         scanner.close();
     }
+    public static double perimetroRettangolo (double l1,double l2){
+        return 2*(l1+l2);
+
+    }
+
 }
